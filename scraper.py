a728ee2ea642c66accd877bf10d36ca7efccd230
ivan lambev <ivan2007ny@gmail.com>
2023-05-05 11:41:12 +0300

    Added Proxys fixed permission errors

--- scraper.py
+++ scraper.py
@@ -2,16 +2,23 @@ import bs4
 import requests
 import psutil
 import os
-
+import random
 import summary
 
 default_file_path = os.path.join(os.getcwd(), "output_files")
 
 
-
 def get_text(url):
     # Make request to the website
-    response = requests.get(url)
+    with open("settings.txt", "r") as f:
+        use_proxy = f.read()
+    print(use_proxy)
+
+    if use_proxy:
+
+        response = requests.get(url, proxies=get_working_proxy(), timeout=2)
+    else:
+        response = requests.get(url)
 
     # Parse HTML content with BeautifulSoup
     soup = bs4.BeautifulSoup(response.content, 'html.parser')
@@ -22,6 +29,7 @@ def get_text(url):
     return text
 
 
+# validation
 def is_valid_url(url):
     try:
         response = requests.head(url)
@@ -48,12 +56,53 @@ def has_ethernet_access():
     return False
 
 
+# proxy config
+def get_proxies():
+    proxy_url = "https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt"
+    r = requests.get(proxy_url)
+    soup = bs4.BeautifulSoup(r.content, 'html.parser').findAll('td',
+                                                               {'class': 'blob-code blob-code-inner js-file-line'})
+    proxies = [proxy.text for proxy in soup]
+    return proxies
+
+
+def get_random_proxy():
+    return {'https': random.choice(get_proxies())}
+
+
+proxies = get_proxies()
+
+
+def get_working_proxy():
+    for i in range(20):
+        proxy = get_random_proxy()
+        try:
+
+            r = requests.get("https://www.google.com", proxies=proxy, timeout=2)
+            if r.status_code == 200:
+                return proxy
+        except requests.exceptions.RequestException as e:
+            return e
+
+
+# scraping
+def scrape(url):
+    if is_valid_url(url):
+        print("Valid URL")
+        text = get_text(url)
+        return text
+    else:
+        print("Invalid URL")
+        print("Skipping...")
+        return "Invalid URL"
+
+
 def scrape_and_output_to_file(url, file_path, file_name):
     print(url)
 
     if is_valid_url(url):
         print("Valid URL")
-        text = get_text(url)
+        text = scrape(url)
         scraped_file_name = file_name + str(i) + ".txt"
         file_pathname = os.path.join(file_path, scraped_file_name)
         if os.path.isfile(file_pathname):
@@ -68,7 +117,6 @@ def scrape_and_output_to_file(url, file_path, file_name):
         else:
             pass
 
-        
         with open(file_pathname, "w") as f:
             f.write(str(text.encode("utf-8")))
             f.close()
@@ -80,35 +128,28 @@ def scrape_and_output_to_file(url, file_path, file_name):
         return "Invalid URL"
 
 
-def summarize(path_to_file, summary_file_name, summary_file_path):
-    if is_valid_path(path_to_file):
-        os.chmod(path_to_file, 0o777)
-        os.chmod(file_path, 0o777)   # set permissions of the output_files folder
-           # set permissions of the file to be written
-        with open(path_to_file, "r") as f:
-            text_to_summarize = f.read()
-            summarized_text = summary.summarize_text(text_to_summarize)
-            default_file_name = "file_summarized"
+def summarize(text, summary_file_name, summary_file_path):
+    text_to_summarize = text
+    summarized_text = summary.summarize_text(text_to_summarize)
+    default_file_name = "file_summarized"
 
-            if summary_file_path == "":
-                summary_file_path = default_file_path
-            else:
-                pass
-
-            if summary_file_name == "":
-                summary_file_name = default_file_name
-            else:
-                pass
-
-            file_name = summary_file_name + ".txt"
-            file_pathname = os.path.join(summary_file_path, file_name)
-            
-            with open(file_pathname, "w") as f:
-
-                f.write(str(summarized_text.encode("utf-8")))
-                f.close()
+    if summary_file_path == "":
+        summary_file_path = default_file_path
     else:
-        return "Invalid path to file"
+        pass
+
+    if summary_file_name == "":
+        summary_file_name = default_file_name
+    else:
+        pass
+
+    file_name = summary_file_name + ".txt"
+    file_pathname = os.path.join(summary_file_path, file_name)
+
+    with open(file_pathname, "w") as f:
+
+        f.write(str(summarized_text.encode("utf-8")))
+        f.close()
 
 
 if __name__ == "__main__":
@@ -120,14 +161,14 @@ if __name__ == "__main__":
     # welcome message "scraper"
     print('''
 
- __          __  _        _____                                      
- \ \        / / | |      / ____|                                     
-  \ \  /\  / /__| |__   | (___   ___ _ __ __ _ _ __  _ __   ___ _ __ 
+ __          __  _        _____
+ \ \        / / | |      / ____|
+  \ \  /\  / /__| |__   | (___   ___ _ __ __ _ _ __  _ __   ___ _ __
    \ \/  \/ / _ \ '_ \   \___ \ / __| '__/ _` | '_ \| '_ \ / _ \ '__|
-    \  /\  /  __/ |_) |  ____) | (__| | | (_| | |_) | |_) |  __/ |   
-     \/  \/ \___|_.__/  |_____/ \___|_|  \__,_| .__/| .__/ \___|_|   
-                                              | |   | |              
-                                              |_|   |_|              
+    \  /\  /  __/ |_) |  ____) | (__| | | (_| | |_) | |_) |  __/ |
+     \/  \/ \___|_.__/  |_____/ \___|_|  \__,_| .__/| .__/ \___|_|
+                                              | |   | |
+                                              |_|   |_|
 
     ''')
 
@@ -141,7 +182,8 @@ if __name__ == "__main__":
     else:
         pass
 
-    print("Please enter mode \n 1 - scrape and output to a file  \n 2 - scrape and summarize \n 3 - exit")
+    print(
+        "Please enter mode \n 1 - scrape and output to a file  \n 2 - scrape and summarize \n 3 - settings \n 4 - exit")
     mode = input("Enter mode: ")
 
     if mode == "1":
@@ -153,8 +195,6 @@ if __name__ == "__main__":
             print(is_valid_url(url))
             file_path = input("Enter file path: ")
             file_name = input("Enter file name: ")
-
-
 
             default_file_name = "file_"
 
@@ -199,8 +239,21 @@ if __name__ == "__main__":
                 pass
 
             print("Starting...")
-            scrape_and_output_to_file(url, file_path, file_name)
+
+            summarize(scrape(url), file_name, file_path)
             print("Done")
 
-            summarize(file_path, file_name, file_path)
-            print("Done")
+    elif mode == "3":
+        print("Settings Menu")
+        use_proxy = input("Use proxy? (y/n): ")
+        use_proxy = use_proxy.lower()
+        if use_proxy == "y":
+            with open("settings.txt", "w") as f:
+                f.write("use_proxy = True")
+                f.close()
+                pass
+
+        else:
+            go_home = input("Press enter to return to main menu: ")
+            if go_home == "":
+                pass
--- summary.py
+++ summary.py
@@ -1,15 +1,15 @@
 import nlpcloud
 
+
 def summarize_text(text_to_summarize):
     client = nlpcloud.Client("bart-large-cnn", "f771bc4e9647a3607f917eae5b5f609f7b320219", gpu=False, lang="en")
-    response = client.summarization(text_to_summarize, size="small")
+    response = client.summarization(text_to_summarize, size="long")
     summary_text = response['summary_text']
     return summary_text
 
-def translate_text(text_to_translate, target_lang):
 
+def translate_text(text_to_translate, target_lang):
     client = nlpcloud.Client("t5-small", "f771bc4e9647a3607f917eae5b5f609f7b320219", gpu=False, lang="en")
     response = client.translation(text_to_translate, target_lang)
     translated_text = response['translated_text']
     return translated_text
-
